48b9032aa8c9a12a3f3416e7a99eca505e3ce389
playboy9n <playboy9n@gmail.com>
2019-05-13 16:08:23 +0900

    [Finish]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def index
+  	@books = Book.all
+    @book = Book.new
+  end
+
+  def top
+  end
+
+  def new
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.new(books_params)
+  	if book.save
+  	redirect_to book_path(book.id), notice: 'Book was successfully created.'
+  else
+    render :index
+  end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+  def update
+    book = Book.find(params[:id])
+    book.update(books_params)
+    redirect_to book_path(book.id)
+  end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def books_params
+  		params.require(:book).permit(:title, :category, :body)
+  	end
+end
